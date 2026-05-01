ebc7292d67c334985e00bfeea69caa830c2a9e4b
shahd ben hmida <152545736+shhahdddddd@users.noreply.github.com>
2026-05-01 23:06:47 +0100

    Update app.config.ts

--- admin/src/app/app.config.ts
+++ admin/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getMessaging, provideMessaging } from '@angular/fire/messaging';
@@ -9,23 +10,16 @@ import { routes } from './app-routing.module';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { environment } from '../environments/environment';
 
-// ✅ SAFE CHECK
-const isMessagingSupported =
-  typeof window !== 'undefined' &&
-  'serviceWorker' in navigator &&
-  location.protocol === 'https:';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideAnimations(),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
     provideCharts(withDefaultRegisterables()),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-
-    // ✅ Only enable messaging if supported
-    ...(isMessagingSupported
-      ? [provideMessaging(() => getMessaging())]
-      : [])
+    provideMessaging(() => getMessaging())
   ]
 };
